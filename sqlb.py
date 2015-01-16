80f56a7307f6746969a43a5578819598e939c043
Paxson Lim <potinglim@gmail.com>
2015-01-16 19:49:50 +0800

    sqlb added

---
+++ sqlb.py
@@ -0,0 +1,28 @@
+# INSERT Command
+
+# import the sqlite3 library
+import sqlite3
+"""
+# create the conneciton object
+conn = sqlite3.connect("new.db")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+# insert data
+cursor.execute("INSERT INTO population VALUES('New York City','NY',8200000)")
+cursor.execute("INSERT INTO population VALUES('San Francisco', 'CA',800000)")
+
+# commit the changes
+conn.commit()
+
+# close the database connection
+conn.close()
+"""
+
+# alternate way of writing the cote with WITH
+with sqlite3.connect("new.db") as connection:
+	c = connection.cursor()
+	c.execute("INSERT INTO population VALUES('New York City','NY',8200000)")
+	c.execute("INSERT INTO population VALUES('San Francisco', 'CA',800000)")
+
